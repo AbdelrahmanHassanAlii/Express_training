9f9aa30c32d0ef4cb8a83115eb9bbec5ebf67f5a
AbdelrahmanHassanAlii <abdo30201080101694@gmail.com>
2025-03-04 14:19:44 +0200

    clean app js file by removing the code we did't use from it

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 const express = require("express");
-const fs = require("fs");
 const morgan = require("morgan");
 
 const userRouter = require("./routes/users");
 const productRouter = require("./routes/products");
 
 const app = express();
-// creating the Routes from the app 
-// const productRouter = express.Router();
-// const userRouter = express.Router();
-
-const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`, "utf-8"));
 
 // Middleware to parse JSON body
 app.use(express.json());
-
-// Middleware to log the request
-// app.use((req, res, next) => {
-//     console.log("Hello from the middleware 🤙!");
-//     next();
-// })
 
 // Middleware to measure the time it takes to execute the request
 app.use((req, res, next) => {
