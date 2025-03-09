fa50f85bca1a41a67de75b63cafb8cca986ab120
AbdelrahmanHassanAlii <abdo30201080101694@gmail.com>
2025-03-09 22:01:00 +0200

    make the dotenv file use all over the app

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+// loading dotenv
+const dotenv = require('dotenv');
+// setting up the environment variables before exporting app and starting the server
+dotenv.config({
+    path: './config.env'
+});
+
 const app = require('./index');
 
 // start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT} 🚀`);
 });
