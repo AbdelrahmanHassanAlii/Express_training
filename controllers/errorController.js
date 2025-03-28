cee97b1b00d8af508f64ead2efaddd835f992836
AbdelrahmanHassanAlii <abdo30201080101694@gmail.com>
2025-03-28 17:19:07 +0200

    refactor the errorHandler component

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,12 +1,38 @@
-exports.errorHandler = (err, req, res, next)=>{
+/* eslint-disable no-console */
+const errorDev = (err, res) => {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        error: err,
+        stack: err.stack
+    })
+}
+
+const errorProd = (err, res) => {
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        })
+    } else {
+        console.error("ERROR IN PRODUCTION 💥", err)
+        res.status(500).json({
+            status: "error",
+            message: "Something went wrong"
+        })
+    }
+}
+
+exports.errorHandler = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.status = err.status || "error";
     err.message = err.message || "Something went wrong";
 
-    res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
-    })
+    if (process.env.NODE_ENV === 'development') {
+        errorDev(err, res)
+    } else if (process.env.NODE_ENV === 'production') {
+        errorProd(err, res)
+    }
 
     next();
 }
