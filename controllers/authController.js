c095096c9243a4de9c13458fc120c3b6435ffabf
AbdelrahmanHassanAlii <abdo30201080101694@gmail.com>
2025-04-20 14:59:45 +0200

    refactor the signup to follow the new structure

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,29 +6,19 @@ const crypto = require('crypto');
 const User = require("../models/userModel")
 const AppError = require("../utils/appError")
 const { catchAsync } = require("../utils/catchAsync")
-const { createUserSchema, updatePasswordSchema } = require("../validators/userValidator");
+const { updatePasswordSchema } = require("../validators/userValidator");
 const sendEmail = require('../utils/email');
 const { createSendToken, sendResponse } = require('../utils/response');
 
 
 exports.signUp = catchAsync(async (req, res, next) => {
-    // validate the input
-    const { error, value } = createUserSchema.validate(req.body);
-    if (error) {
-        return next(new AppError(error.details[0].message, 400));
-    }
+    // get validated data from previous middleware
+    const { name, email, password, passwordConfirm } = req.validatedBody;
     // create user
-    const user = await User.create({
-        name: value.name,
-        email: value.email,
-        password: value.password,
-        passwordConfirm: value.passwordConfirm
-    });
-    // remove password from the user
-    user.password = undefined;
+    const user = await User.create({ name, email, password, passwordConfirm });
     // generate token and return response
     createSendToken(user, 201, res);
-})
+});
 
 exports.logIn = catchAsync(async (req, res, next) => {
     // validate the input
