825e9d4584faee1f46eb38fc64b9f59a802a4f83
AbdelrahmanHassanAlii <abdo30201080101694@gmail.com>
2025-04-21 23:06:13 +0200

    handle updateMyProfile function

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,5 +1,6 @@
 const User = require("../models/userModel");
 const { catchAsync } = require("../utils/catchAsync");
+const filterObj = require("../utils/filterObj");
 const { sendResponse } = require("../utils/response");
 
 // user routes handlers
@@ -35,16 +36,14 @@ exports.deleteUser = (req, res) => {
 }
 
 
-exports.updateUser = (req, res) => {
-    res.status(500).json({
-        status: "error",
-        message: "not implemented yet",
-    })
-}
-
-exports.deleteUser = (req, res) => {
-    res.status(500).json({
-        status: "error",
-        message: "not implemented yet",
-    })
-}
+exports.updateMyProfile = catchAsync(async (req, res, next) => {
+    // 1) Filter out the fields that are not allowed to be updated
+    const filteredBody = filterObj(req.body, 'name', 'email');
+    // 2) update the user
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+        new: true, 
+        runValidators: true 
+    });
+    // 3) send response
+    sendResponse(res, 200, 'User details', updatedUser);
+})
