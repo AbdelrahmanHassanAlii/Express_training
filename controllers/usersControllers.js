d984b40c3d8067bbd8d6d40efe8c881572472ab4
AbdelrahmanHassanAlii <abdo30201080101694@gmail.com>
2025-05-04 17:32:45 +0300

    make updateMyProfile with updateOne

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,8 +1,8 @@
 const User = require("../models/userModel");
 const { catchAsync } = require("../utils/catchAsync");
-const filterObj = require("../utils/filterObj");
+// const filterObj = require("../utils/filterObj");
 const { sendResponse } = require("../utils/response");
-const { deleteOne } = require("./handlerFactory");
+const { deleteOne, updateOne } = require("./handlerFactory");
 
 // user routes handlers
 exports.getAllUsers = catchAsync( async (req, res) => {
@@ -36,14 +36,4 @@ exports.deleteMyProfile = catchAsync(async (req, res, next) => {
     sendResponse(res, 204, 'User deleted', null);
 })
 
-exports.updateMyProfile = catchAsync(async (req, res, next) => {
-    // 1) Filter out the fields that are not allowed to be updated
-    const filteredBody = filterObj(req.body, 'name', 'email');
-    // 2) update the user
-    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
-        new: true, 
-        runValidators: true 
-    });
-    // 3) send response
-    sendResponse(res, 200, 'User details', updatedUser);
-})
+exports.updateMyProfile = updateOne(User)
