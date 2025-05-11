ca66004ff4f290b05207e5e2b72703886ee13420
AbdelrahmanHassanAlii <abdo30201080101694@gmail.com>
2025-05-11 17:52:38 +0300

    handle the dynamic calculation of the number of reviews and the rating avarage on the tour model from the review model and handle this after save, update and delete

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,6 @@
+/* eslint-disable prefer-arrow-callback */
 const { mongoose } = require("mongoose");
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema({
     review: {
@@ -44,6 +46,46 @@ reviewSchema.pre(/^find/, function (next) {
     })
     next();
 })
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId }
+        }
+        ,
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rate' }
+            }
+        }
+    ])
+
+    if (stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].nRating,
+            ratingsAverage: stats[0].avgRating
+        })
+    }else{
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 4.5
+        })
+    }
+}
+
+
+reviewSchema.post('save', function (doc, next) {
+    // this.constructor points to the model
+    doc.constructor.calcAverageRatings(doc.tour);
+    next();
+})
+
+// After updating or deleting a review
+reviewSchema.post(/^findOneAnd/, async function (doc) {
+    if (doc) await doc.constructor.calcAverageRatings(doc.tour);
+});
 
 const Review = mongoose.model('Review', reviewSchema);
 
